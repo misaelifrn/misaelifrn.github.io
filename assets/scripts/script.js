ae8468d5689e75be346a8aa7fdcf0802f203a42c
misaelifrn <misaelifrn33@gmail.com>
2026-04-15 09:41:21 -0300

    Add files via upload

---
+++ assets/scripts/script.js
@@ -0,0 +1,18 @@
+console.log("Funcionando!");
+
+// tema lógica
+const temaBtn = document.getElementById("themeBtn");
+const sol = document.getElementsByClassName("sol");
+const lua = document.getElementsByClassName("lua");
+
+temaBtn.addEventListener('click', function() {
+    alert("a");
+    // if (sol.style.display === block) {
+    //     sol.style.display = "none"; 
+    //     lua.style.display = "block";
+    // } else {
+    //     sol.style.display = "block"; 
+    //     lua.style.display = "none";
+    // }
+});
+
